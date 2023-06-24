ed5e3c1632ea54685c5a4f96e988637cfc7a379a
IamCOD3X <sourabh.pitanjli28@gmail.com>
2023-06-25 03:02:43 +0530

    Making viper changes

--- gcc.sh
+++ gcc.sh
@@ -2,33 +2,36 @@
 echo "Cloning dependencies"
 git clone https://github.com/mvaisakh/gcc-arm64 --depth=1
 git clone https://github.com/mvaisakh/gcc-arm --depth=1
-git clone --depth=1 https://gitlab.com/Baibhab34/AnyKernel3.git -b rmx1801 AnyKernel
+git clone --depth=1 https://github.com/IamCOD3X/AnyKernel3.git AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
-export CONFIG_PATH=$PWD/arch/arm64/configs/RMX1801_defconfig
+export CONFIG_PATH=$PWD/arch/arm64/configs/viper_nethunter_defconfig
 PATH="$(pwd)/gcc-arm64/bin:$(pwd)/gcc-arm/bin:${PATH}" \
 export ARCH=arm64
 export USE_CCACHE=1
 export KBUILD_BUILD_HOST=GCC
-export KBUILD_BUILD_USER="baibhab"
+export KBUILD_BUILD_USER="COD3X"
+# Kernel local name
+export LOCALVERSION=-ViP3R🐍-v1.0-NetHunter-2k23
 
 # Compile plox
 function compile() {
-   make O=out ARCH=arm64 RMX1801_defconfig
+   make O=out ARCH=arm64 viper_nethunter_defconfig 
      PATH="$(pwd)/gcc-arm64/bin:$(pwd)/gcc-arm/bin:${PATH}" \
        make -j$(nproc --all) O=out \
                              ARCH=arm64 \
                              CROSS_COMPILE=aarch64-elf- \
                              CROSS_COMPILE_ARM32=arm-eabi-
+                             CONFIG_DEBUG_SECTION_MISMATCH=y
    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-   zip -r9 Avalanche-RMX1801-EAS-${TANGGAL}.zip *
-    curl https://bashupload.com/Avalanche-RMX1801-EAS-${TANGGAL}.zip --data-binary @Avalanche-RMX1801-EAS-${TANGGAL}.zip
+   zip -r9 ViP3R-RMX1801-EAS-${TANGGAL}.zip *
+    curl https://bashupload.com/ViP3R-RMX1801-EAS-${TANGGAL}.zip --data-binary @ViP3R-RMX1801-EAS-${TANGGAL}.zip
     cd ..
 }
 compile
